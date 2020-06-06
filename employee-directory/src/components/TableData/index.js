84470bad7bbf06aee828eb22d81eb47a31b5c1ba
Arthur Freeman <Arthur@AJ-Freeman-MacBook.local>
2020-06-06 11:56:42 -0400

    Add initial table data with temporary rows

---
+++ employee-directory/src/components/TableData/index.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+function TableData() {
+  return (
+    <tbody>
+      <tr>
+        <td>Arthur</td>
+        <td>Arthur</td>
+        <td>Arthur</td>
+        <td>Arthur</td>
+        <td>Arthur</td>
+        <td>Arthur</td>
+      </tr>
+
+      <tr>
+        <td>Arthur</td>
+        <td>Arthur</td>
+        <td>Arthur</td>
+        <td>Arthur</td>
+        <td>Arthur</td>
+        <td>Arthur</td>
+      </tr>
+
+      <tr>
+        <td>Arthur</td>
+        <td>Arthur</td>
+        <td>Arthur</td>
+        <td>Arthur</td>
+        <td>Arthur</td>
+        <td>Arthur</td>
+      </tr>
+
+      <tr>
+        <td>Arthur</td>
+        <td>Arthur</td>
+        <td>Arthur</td>
+        <td>Arthur</td>
+        <td>Arthur</td>
+        <td>Arthur</td>
+      </tr>
+
+      <tr>
+        <td>Arthur</td>
+        <td>Arthur</td>
+        <td>Arthur</td>
+        <td>Arthur</td>
+        <td>Arthur</td>
+        <td>Arthur</td>
+      </tr>
+
+      <tr>
+        <td>Arthur</td>
+        <td>Arthur</td>
+        <td>Arthur</td>
+        <td>Arthur</td>
+        <td>Arthur</td>
+        <td>Arthur</td>
+      </tr>    
+    </tbody>
+  )
+}
+
+export default TableData;
