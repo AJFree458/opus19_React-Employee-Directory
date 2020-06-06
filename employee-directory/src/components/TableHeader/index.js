b52e34d63da6d25feacb5f3c1e9d54cbb6bbf81b
Arthur Freeman <Arthur@AJ-Freeman-MacBook.local>
2020-06-06 11:57:41 -0400

    Add initial table header component with temporary heads

---
+++ employee-directory/src/components/TableHeader/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function TableHeader() {
+  return (
+    <thead className="thead-dark">
+          <th scope="col">Name</th>
+          <th scope="col">Name</th>
+          <th scope="col">Name</th>
+          <th scope="col">Name</th>
+          <th scope="col">Name</th>
+          <th scope="col">Name</th>
+        </thead>
+
+  )
+}
+
+export default TableHeader;
