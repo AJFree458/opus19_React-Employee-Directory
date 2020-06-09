069de4fcafc564efbe414c03d7980945717eb15f
Arthur Freeman <Arthur@AJ-Freeman-MacBook.local>
2020-06-08 20:08:14 -0400

    Updates

---
+++ precache-manifest.0663a1e6947db5effe608b629df1008b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf9b61f706269cf869f5dc36f82e3cd",
+    "url": "/opus19_React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "aefa628884cfadaf3269",
+    "url": "/opus19_React-Employee-Directory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "98224b7ac5c6f839d4c8",
+    "url": "/opus19_React-Employee-Directory/static/js/2.ebd1d1df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/opus19_React-Employee-Directory/static/js/2.ebd1d1df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aefa628884cfadaf3269",
+    "url": "/opus19_React-Employee-Directory/static/js/main.c487430b.chunk.js"
+  },
+  {
+    "revision": "4a90dc2d23902211e8cf",
+    "url": "/opus19_React-Employee-Directory/static/js/runtime-main.8f4e698a.js"
+  }
+]);
